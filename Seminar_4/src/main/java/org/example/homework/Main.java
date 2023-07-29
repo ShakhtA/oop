6b534f11252a10c75db2b714b123e6929cd92f32
Andrey <shakht_a@mail.ru>
2023-07-29 17:37:56 +0300

    ДобÐРеализованы классы Fruit, Apple и Orange

--- Seminar_4/src/main/java/org/example/homework/Main.java
+++ Seminar_4/src/main/java/org/example/homework/Main.java
@@ -2,7 +2,9 @@ package org.example.homework;
 
 public class Main {
     public static void main(String[] args) {
-
-
+        Apple apple1 = new Apple();
+        System.out.println(apple1);
+        Orange orange1 = new Orange();
+        System.out.println(orange1);
     }
 }
